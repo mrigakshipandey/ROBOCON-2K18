89d65521357a98286d7a6f6a23dd72980cf3cb8d
Myki1248 <mayankgupta1248@gmail.com>
2017-11-04 17:13:37 +0530

    a

--- ps3/ps3.ino
+++ ps3/ps3.ino
@@ -14,6 +14,10 @@
 
 #include <SPI.h>
 #include <PS3BT.h>
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
 
 USB Usb;
 BTD Btd(&Usb);
@@ -30,8 +34,10 @@ int m31 = 7; //BL
 int m32 = 8;
 int m41 = 5; //BR
 int m42 = 6;
-
-
+boolean b1 = false,b2 = false;
+int s1 = 44;
+int s2 = 45;
+int posAtt1 = 140,posAtt2 = 130,posRel1 = 0,posRel2 = 0;
 void setup() {
 
    Serial.begin(115200);
@@ -40,7 +46,11 @@ void setup() {
     Serial.print(F("\r\nOSC did not start"));
     while (1); //halt
   }
-
+  servo1.attach(s1);
+  servo2.attach(s2);
+  servo1.write(posAtt1);
+  servo2.write(posAtt2);
+  
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
   pinMode(m11, OUTPUT);
   pinMode(m21, OUTPUT);
@@ -66,18 +76,33 @@ void loop() {
     y1 =  map(PS3.getAnalogHat(LeftHatY), 0, 255, -122, 123);
     x2 =  map(PS3.getAnalogHat(RightHatX), 0, 255, -122, 123);
     y2 =  map(PS3.getAnalogHat(RightHatY), 0, 255, -122, 123);
-
+    b1 = PS3.getButtonClick(L1);
+    b2 = PS3.getButtonClick(R1);
+    
     Serial.println(x1);
     Serial.println(y1);
     Serial.println(x2);
     Serial.println(y2);
-
+    Serial.println(b1);
+    Serial.println(b2);
+    
     s = sqrt(2);
     p1 = x1-y1/s;
     p2 = -x1-y1/s;
     p3 = x1-y1/s;
     p4 = -x1-y1/s;
-  
+
+    if(b1)
+    {
+      servo1.write(posAtt1);
+      servo2.write(posAtt2);
+    }
+    if(b2)
+    {
+      servo1.write(posRel1);
+      servo2.write(posRel2);
+    }
+    
     if (x2 == 0) {
     if (p1>0) {
       Serial.println("I");
